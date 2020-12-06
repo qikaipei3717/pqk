1519ff738de49f47932ae20531c70362c0cf8458
qikaipei <pqk771048904@163.com>
2020-12-06 22:25:10 +0800

    fsagds

---
+++ 各类账号密码.sh
@@ -0,0 +1,21 @@
+阿里云服务器 
+实例ID   i-m5e7b9j50uug8xdmxoei
+公网IP   47.104.131.118
+账号     root
+密码     Pei033717
+CPU&内存2 核8 GiB
+操作系统   CentOS 7.5 64位
+
+阿里云服务器ftp服务器
+adduser ftppei
+passwd pei033717
+
+ftp://47.104.131.118:21
+阿里云服务器mysql
+账户  root
+密码  Hadoop*123
+
+github
+用户名 qikaipei
+邮箱 pqk771048904@163.com
+密码 pei033717
